7e34f33eacb56d5df3c298828bb4df51edcdfe41
David <dclark@MacBook-Air.local>
2017-07-28 11:59:45 -0400

    vorticity_wavenumbers_spectral_sum_comparison.m:  ring averaged vort

--- funwaveC/vorticity_wavenumbers_spectral_sum_comparison.m
+++ funwaveC/vorticity_wavenumbers_spectral_sum_comparison.m
@@ -20,5 +20,36 @@ xi_frf = [xi_frf, xi_frf(end)-dx] + dx/2;
 numx = 585;
 numy = 1200;
 
-%%
+
+%% ring averaged vorticity 
+
+numfiles = 1900;
+% make averaging areas
+RING = struct;
+for ii=1:25;
+    RING(ii).bin_length = (ii-1)*2+1; % odd numbers
+    RING(ii).i = (iring-(ii-1)):(iring+(ii-1));
+    RING(ii).j = 1:RING(ii).bin_length;
+    RING(ii).steps = floor(numy/RING(ii).bin_length);  
+    RING(ii).vort = zeros(numfiles,RING(ii).steps);
+end
+
+datadir = '/Volumes/ThunderBay/fC_RODSEX_0928_D3_dx1p3/';
+% datadir = '~/Dropbox/RODSEX/funwaveC/';
+
+
+n=0;
+for ii = 3599:(3599+numfiles)
+    n=n+1;
+    fprintf('%g\n',ii)
+    load(sprintf('%ssnap_vort_l2_%4.0f.mat',datadir,ii))
+
+    for jj = 1:length(RING) 
+        for kk = 0:(RING(jj).steps-1);
+            RING(jj).vort(n,kk+1) = sum(sum(vort(RING(jj).i,RING(jj).j + (RING(jj).bin_length*kk))))/(RING(jj).bin_length.^2);
+        end
+    end
+ 
+end
+
 
